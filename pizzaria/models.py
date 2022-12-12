f920e75d04014afba8cf7d8bec030a6d77bdb1d3
campbell <campbell.com>
2022-12-11 21:34:19 -0600

    doing my best

--- pizzaria/models.py
+++ pizzaria/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Pizza(models.Model):
     pizza_name = models.CharField(max_length=20)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    image = models.ImageField(upload_to='img',blank=True,null=True)
 
     def __str__(self):
         return self.pizza_name
